5bb0e5d6d8bf87ebd2a25de8c01dc43935b56fab
danylo_verenych <karaten3@gmail.com>
2023-12-18 11:12:40 +0200

    feat: delete not neede files from backend branch

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ErrorMessage.scss";
-
-const ErrorMessage = ({ errorMessage }) => {
-  return <p className="error">{errorMessage}</p>;
-};
-
-export default ErrorMessage;
