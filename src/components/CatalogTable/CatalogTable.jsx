5e54e82aa9aa4291c7e827da4b21548e1585b3c1
_PaLa4_ <illya.kis99@gmail.com>
2024-01-19 13:32:32 +0200

    added search input

--- src/components/CatalogTable/CatalogTable.jsx
+++ src/components/CatalogTable/CatalogTable.jsx
@@ -7,6 +7,7 @@ const CatalogTable = ({ user }) => {
     field: null,
     ascending: true,
   });
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchDataFromDB = async () => {
@@ -37,7 +38,44 @@ const CatalogTable = ({ user }) => {
     });
   };
 
-  const sortedData = catalogData.slice().sort((a, b) => {
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const filteredData = catalogData.filter((row) => {
+    const lowerCaseQuery = searchQuery.toLowerCase();
+
+    return Object.entries(row).some(([key, value]) => {
+      const lowerCaseValue = value ? value.toString().toLowerCase() : '';
+
+      switch (key) {
+        case 'date_work':
+          if (!isNaN(new Date(value).getTime())) {
+            const formattedDate = formatDate(value);
+            return formattedDate.includes(lowerCaseQuery);
+          }
+          break;
+
+        case 'is_doc':
+          return lowerCaseQuery === '' || (lowerCaseQuery === '+' && value) || (lowerCaseQuery === '-' && !value);
+
+        case 'address':
+          return lowerCaseValue.includes(lowerCaseQuery) || lowerCaseQuery === '';
+
+        case 'type_work':
+          return lowerCaseValue.includes(lowerCaseQuery);
+
+        case 'id_doc':
+          return lowerCaseValue.includes(lowerCaseQuery) || lowerCaseQuery === 'не документ';
+
+        default:
+          return false;
+      }
+    });
+  });
+
+
+  const sortedData = filteredData.slice().sort((a, b) => {
     const aValue = sortOrder.field ? a[sortOrder.field] : null;
     const bValue = sortOrder.field ? b[sortOrder.field] : null;
 
@@ -53,42 +91,65 @@ const CatalogTable = ({ user }) => {
   });
 
   return (
-    <table className='catalogTable'>
-      <thead>
-        <tr className='catalogTable__tr'>
-          <th className='catalogTable__th' onClick={() => handleSort('date_work')}>
-            Дата роботи
-          </th>
-          <th className='catalogTable__th' onClick={() => handleSort('is_doc')}>
-            Документ
-          </th>
-          <th className='catalogTable__th' onClick={() => handleSort('address')}>
-            Адреса
-          </th>
-          <th className='catalogTable__th' onClick={() => handleSort('id_doc')}>
-            ID документа
-          </th>
-          <th className='catalogTable__th' onClick={() => handleSort('type_work')}>
-            Тип робіт
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedData.length > 0 ? (
-          sortedData.map((row, index) => (
-            <tr key={index} className='catalogTable__tr'>
-              <td className='catalogTable__td'>{formatDate(row.date_work)}</td>
-              <td className='catalogTable__td'>{row.is_doc ? '+' : '-'}</td>
-              <td className='catalogTable__td'>{row.address}</td>
-              <td className='catalogTable__td'>{row.id_doc ? row.id_doc : 'Не документ'}</td>
-              <td className='catalogTable__td'>{row.type_work}</td>
-            </tr>
-          ))
-        ) : (
-          <span className='catalogTable__user'>Користувач не залогінений</span>
-        )}
-      </tbody>
-    </table>
+    <div>
+      <input
+        type="text"
+        placeholder="Пошук"
+        value={searchQuery}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      <table className='catalogTable'>
+        <thead>
+          <tr className='catalogTable__tr'>
+            <th
+              className={`catalogTable__th ${sortOrder.field === 'date_work' ? `catalogTable__th-${sortOrder.ascending ? 'asc' : 'desc'}` : ''}`}
+              onClick={() => handleSort('date_work')}
+            >
+              Дата роботи
+            </th>
+            <th
+              className={`catalogTable__th ${sortOrder.field === 'is_doc' ? `catalogTable__th-${sortOrder.ascending ? 'asc' : 'desc'}` : ''}`}
+              onClick={() => handleSort('is_doc')}
+            >
+              Документ
+            </th>
+            <th
+              className={`catalogTable__th ${sortOrder.field === 'address' ? `catalogTable__th-${sortOrder.ascending ? 'asc' : 'desc'}` : ''}`}
+              onClick={() => handleSort('address')}
+            >
+              Адреса
+            </th>
+            <th
+              className={`catalogTable__th ${sortOrder.field === 'id_doc' ? `catalogTable__th-${sortOrder.ascending ? 'asc' : 'desc'}` : ''}`}
+              onClick={() => handleSort('id_doc')}
+            >
+              ID документа
+            </th>
+            <th
+              className={`catalogTable__th ${sortOrder.field === 'type_work' ? `catalogTable__th-${sortOrder.ascending ? 'asc' : 'desc'}` : ''}`}
+              onClick={() => handleSort('type_work')}
+            >
+              Тип робіт
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.length > 0 ? (
+            sortedData.map((row, index) => (
+              <tr key={index} className='catalogTable__tr'>
+                <td className='catalogTable__td'>{formatDate(row.date_work)}</td>
+                <td className='catalogTable__td'>{row.is_doc ? '+' : '-'}</td>
+                <td className='catalogTable__td'>{row.address}</td>
+                <td className='catalogTable__td'>{row.id_doc ? row.id_doc : 'Не документ'}</td>
+                <td className='catalogTable__td'>{row.type_work}</td>
+              </tr>
+            ))
+          ) : (
+            <span className='catalogTable__user'>{user.length > 0 ? 'Нічого не знайдено' : "Користувач не залогінений"}</span>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
