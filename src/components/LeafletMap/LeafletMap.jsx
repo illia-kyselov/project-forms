8a7b7e1f21875c6c087decbc1c6034adf0a2709a
_PaLa4_ <illya.kis99@gmail.com>
2024-02-12 17:30:54 +0200

    added new tilelayer

--- src/components/LeafletMap/LeafletMap.jsx
+++ src/components/LeafletMap/LeafletMap.jsx
@@ -349,6 +349,15 @@ const LeafletMap = ({
               ext="png"
             />
           </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Building Map">
+            <TileLayer
+              url="https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}"
+              minZoom={0}
+              maxZoom={20}
+              attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              ext="png"
+            />
+          </LayersControl.BaseLayer>
           <Overlay name="Markers" checked>
             <MarkerClusterGroup disableClusteringAtZoom={18}>
               {isChecked ? (
